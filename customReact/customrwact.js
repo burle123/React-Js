b5e4181c59dd2cd88d8c65f75de4df7c497931fc
burle123 <burleshantanu@gmail.com>
2026-05-30 14:10:49 +0530

    Custom React

---
+++ customReact/customrwact.js
@@ -0,0 +1,10 @@
+const reactElement = {
+    type : 'a',
+    prop : {
+        href : 'https:/google.com',
+        target : '_blank'
+    },
+    children : 'click me to visit google'
+}
+
+const mainCotainer = document.getElementById("root")
