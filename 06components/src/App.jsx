ffc89fbf63b91b612592408bbcefb79d4841b687
burle123 <burleshantanu@gmail.com>
2026-05-30 14:06:50 +0530

    React Components

--- 06components/src/App.jsx
+++ 06components/src/App.jsx
@@ -8,10 +8,10 @@ function App() {
     <>
       <Navbar />
       <div className="cards">
-        <Card title="card 1" description="card 1 desc" />
-        <Card title="card 2" description="card 2 desc" />
-        <Card title="card 3" description="card 3 desc" />
-        <Card title="card 4" description="card 4 desc" />
+        <Card title="card 1" description="card 1 desc" style={{ backgroundColor: "lightgray" }} imgSrc="https://th.bing.com/th/id/OIP.JPogPMSOOv8g9WJdtLUluAHaEe?w=245&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" />
+        <Card title="card 2" description="card 2 desc" style={{ backgroundColor: "lightgray" }} imgSrc="https://cdn.shopify.com/s/files/1/0306/6419/6141/articles/coding_languages.png?v=1619126283" />
+        <Card title="card 3" description="card 3 desc" style={{ backgroundColor: "lightgray" }} imgSrc="https://cdn.shopify.com/s/files/1/0306/6419/6141/articles/coding_languages.png?v=1619126283" />
+        <Card title="card 4" description="card 4 desc" style={{ backgroundColor: "lightgray" }} imgSrc="https://th.bing.com/th/id/OIP.JPogPMSOOv8g9WJdtLUluAHaEe?w=245&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" />
       </div>
       <Footer />
     </>
