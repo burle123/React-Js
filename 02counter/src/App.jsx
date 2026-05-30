38c0a8861936f4cab5e85838f04b0e667c066bd6
burle123 <burleshantanu@gmail.com>
2026-05-30 14:14:39 +0530

    Counter fn

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter]  = useState(15)
+
+  //let counter = 15
+
+  const addValue = () => {
+    if(counter == 20) {
+     setCounter(20) 
+     alert("Counter value cannot be more than 20") 
+    }
+    else {
+      setCounter(counter + 1)
+    } 
+  }
+
+  const removeValue = () => {
+    if(counter == 0) {
+      setCounter(0)
+      alert("Counter value cannot be less than 0")
+    }
+    else{
+      setCounter(counter - 1)
+
+    }     
+  }
+  
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add value {counter}</button> 
+      <br />
+      <br />
+      <button
+      onClick={removeValue}
+      >remove value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
